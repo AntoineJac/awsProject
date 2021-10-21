afc120df83876f8180f173254d22d282a05e97b2
Antoine Jacquemin <ajacquemin@ajacquemin-ltm.internal.salesforce.com>
2021-10-21 19:04:45 +0200

    publishFunction

---
+++ lambdas/publishEntry/app.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+const utilsLayer = require('/opt/nodejs/index');
+/* eslint-enable */
+
+exports.lambdaHandler = async (event, context, callback) => {
+    await utilsLayer.processMC(event.body, 'returnObj')
+    .then((response) => context.succeed(response))
+    .catch((err) => callback(null, {
+        statusCode: err.code || 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: `##ERROR: ErrorName: ${err.name}  -  ErrorMessage: ${err.message}`,
+        }),
+    }));
+};
